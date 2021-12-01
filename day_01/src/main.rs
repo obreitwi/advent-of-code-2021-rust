5868b104851e1c3824b03b87cdcc8f1de17ebd59
Oliver Breitwieser <oliver@breitwieser.eu>
2021-12-01 11:58:13 +0100

    Day 01 part 02

--- day_01/src/main.rs
+++ day_01/src/main.rs
@@ -5,14 +5,10 @@ use std::io::{self, BufRead};
 use std::path::{Path, PathBuf};
 
 fn main() -> Result<()> {
-    let input = PathBuf::from(
-        env::args()
-            .nth(1)
-            .with_context(|| "No input provided!")?,
-    );
+    let input = PathBuf::from(env::args().nth(1).with_context(|| "No input provided!")?);
     println!("Input: {}", input.display());
     part1(&input)?;
-    // part2(&input)?;
+    part2(&input)?;
     Ok(())
 }
 
@@ -34,5 +30,47 @@ fn part1(input: &Path) -> Result<()> {
         prev = Some(num);
     }
     println!("Part 1: {} increases", count_increases);
+    Ok(())
+}
+
+fn part2(input: &Path) -> Result<()> {
+    let mut count_increases: usize = 0;
+
+    let mut lines = io::BufReader::new(File::open(input)?).lines().enumerate();
+
+    let parse = |line: String| line.parse::<usize>();
+    let first = lines
+        .next()
+        .with_context(|| "First line missing.")
+        .map(|t| t.1)?
+        .map(parse)??;
+    let second = lines
+        .next()
+        .with_context(|| "Second line missing")
+        .map(|t| t.1)?
+        .map(parse)??;
+    let third = lines
+        .next()
+        .with_context(|| "Third line missing")
+        .map(|t| t.1)?
+        .map(parse)??;
+
+    let mut window = vec![third, second, first];
+
+    for (i, line) in io::BufReader::new(File::open(input)?).lines().enumerate() {
+        let num = line
+            .with_context(|| format!("Could not read line {}", i))?
+            .parse::<usize>()?;
+
+        let sum_prev: usize = window.iter().sum();
+        window.pop();
+        window.insert(0, num);
+        let sum_now: usize = window.iter().sum();
+
+        if sum_prev < sum_now {
+            count_increases += 1;
+        }
+    }
+    println!("Part 2: {} increases", count_increases);
     Ok(())
 }
