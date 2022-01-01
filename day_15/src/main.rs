e1b8543592a62e8ce02a6f67c3f6d6693b2887a0
Oliver Breitwieser <oliver@breitwieser.eu>
2022-01-01 14:21:22 +0100

    Day 15 part 01

--- day_15/src/main.rs
+++ day_15/src/main.rs
@@ -1,4 +1,5 @@
 #![allow(unused_imports)]
+#![feature(map_first_last)]
 use anyhow::{bail, Context, Error, Result};
 use nom::{
     branch::alt,
@@ -12,9 +13,9 @@ use nom::{
     Err::{Failure, Incomplete},
     ErrorConvert, Finish, IResult,
 };
-use std::cmp::Ordering;
+use std::cmp::{Ord, Ordering, PartialOrd};
 use std::collections::hash_map::Entry;
-use std::collections::{HashMap, HashSet, VecDeque};
+use std::collections::{BTreeSet, HashMap, HashSet, VecDeque};
 use std::env;
 use std::fmt;
 use std::fs::read_to_string;
@@ -38,41 +39,37 @@ fn main() -> Result<()> {
 }
 
 fn part1(grid: &Grid) {
-    let low_points = grid.get_low_points();
-
+    let parent_cum_risk = grid.get_lowest_risk_paths();
     println!(
-        "part1: {} low points with risk of {}",
-        low_points.len(),
-        low_points.iter().map(|p| p.risk_level()).sum::<usize>()
-    );
+        "part 1: {}",
+        parent_cum_risk[grid.size_y - 1][grid.size_x - 1].risk
+    )
 }
 
-fn part2(grid: &Grid) {
-    println!("part 2: product: {}", grid.get_largest_basins().iter().product::<usize>());
-}
+fn part2(grid: &Grid) {}
 
 #[derive(Debug, Clone)]
 struct Grid {
     size_x: usize,
     size_y: usize,
-    grid: Vec<Vec<usize>>,
+    risk: Vec<Vec<usize>>,
 }
 
 impl Parseable for Grid {
     fn parse(i: &str) -> IResult<&str, Self> {
-        let (i, grid) = separated_list1(line_ending, many1(num1::<usize>))(i)?;
+        let (i, risk) = separated_list1(line_ending, many1(num1::<usize>))(i)?;
 
-        let size_y = grid.len();
-        let size_x = grid[0].len();
+        let size_y = risk.len();
+        let size_x = risk[0].len();
 
-        for line in grid.iter() {
+        for line in risk.iter() {
             assert_eq!(line.len(), size_x);
         }
 
         Ok((
             i,
             Self {
-                grid,
+                risk,
                 size_x,
                 size_y,
             },
@@ -89,105 +86,125 @@ impl Grid {
         }
     }
 
-    fn get_low_points(&self) -> Vec<Point> {
-        let mut points = Vec::new();
+    fn get_lowest_risk_paths(&self) -> Vec<Vec<Point>> {
+        // encode which point is the parent and the cumulative costs
+        let mut parent_cum_risk: Vec<Vec<Point>> = vec![
+            vec![
+                Point {
+                    x: usize::MAX,
+                    y: usize::MAX,
+                    risk: usize::MAX
+                };
+                self.size_x
+            ];
+            self.size_y
+        ];
+        let mut queue: BTreeSet<Point> = BTreeSet::new();
+        queue.insert(Point {
+            x: 0,
+            y: 0,
+            risk: 0,
+        });
+        parent_cum_risk[0][0] = Point {
+            x: 0,
+            y: 0,
+            risk: self.risk[0][0],
+        };
 
-        for y in 0..self.size_y {
-            for x in 0..self.size_x {
-                let height = self.grid[y][x];
+        while let Some(current) = queue.pop_first() {
+            // check if we already found a better alternative
+            if parent_cum_risk[current.y][current.x].risk < current.risk {
+                assert_ne!(parent_cum_risk[current.y][current.x].risk, usize::MAX);
+                // continue;
+            }
 
-                if y > 0 && self.grid[y - 1][x] <= height {
-                    continue;
-                }
-                if x > 0 && self.grid[y][x - 1] <= height {
-                    continue;
-                }
-                if x < self.size_x - 1 && self.grid[y][x + 1] <= height {
-                    continue;
-                }
-                if y < self.size_y - 1 && self.grid[y + 1][x] <= height {
-                        continue;
-                }
-                points.push(Point { x, y, height });
+            if current.x > 0 {
+                self.update(
+                    &current,
+                    current.x - 1,
+                    current.y,
+                    &mut queue,
+                    &mut parent_cum_risk,
+                );
+            }
+            if current.y > 0 {
+                self.update(
+                    &current,
+                    current.x,
+                    current.y - 1,
+                    &mut queue,
+                    &mut parent_cum_risk,
+                );
+            }
+            if current.x < self.size_x - 1 {
+                self.update(
+                    &current,
+                    current.x + 1,
+                    current.y,
+                    &mut queue,
+                    &mut parent_cum_risk,
+                );
+            }
+            if current.y < self.size_y - 1 {
+                self.update(
+                    &current,
+                    current.x,
+                    current.y + 1,
+                    &mut queue,
+                    &mut parent_cum_risk,
+                );
             }
         }
-
-        points
+        assert_eq!(queue.len(), 0);
+        parent_cum_risk
     }
 
-    fn get_basin(&self, point: &Point) -> HashSet<(usize, usize)> {
-        let origin = (point.x, point.y);
-
-        let mut basin_queue = BasinQueue::new(self);
-        basin_queue.checked_push(origin);
-
-        while let Some(current) = basin_queue.pop_front() {
-            if current.1 > 0 {
-                basin_queue.checked_push((current.0, current.1 - 1));
-            }
-            if current.0 > 0 {
-                basin_queue.checked_push((current.0 - 1, current.1));
-            }
-            if current.0 < self.size_x - 1 {
-                basin_queue.checked_push((current.0 + 1, current.1));
-            }
-            if current.1 < self.size_y - 1 {
-                basin_queue.checked_push((current.0, current.1 + 1));
-            }
+    // check if given point is reachable from parent Point with lower totla risk and update
+    // accordingly
+    fn update(
+        &self,
+        parent: &Point,
+        new_x: usize,
+        new_y: usize,
+        queue: &mut BTreeSet<Point>,
+        parent_cum_risk: &mut Vec<Vec<Point>>,
+    ) {
+        let risk_current = parent_cum_risk[new_y][new_x].risk;
+        let risk_step = self.risk[new_y][new_x];
+        let risk_new = parent.risk + risk_step;
+        if risk_new < risk_current {
+            parent_cum_risk[new_y][new_x] = Point {
+                risk: risk_new,
+                ..*parent
+            };
+            queue.insert(Point {
+                x: new_x,
+                y: new_y,
+                risk: risk_new,
+            });
         }
-
-        basin_queue.basin
-    }
-
-    fn get_basin_sizes(&self) -> Vec<usize> {
-        self.get_low_points().iter().map(|p| self.get_basin(p).len()).collect()
-    }
-
-    fn get_largest_basins(&self) -> Vec<usize> {
-        let mut basin_sizes = self.get_basin_sizes();
-        basin_sizes.sort_unstable();
-        let all_but_three = basin_sizes.len() - 3;
-        basin_sizes.into_iter().skip(all_but_three).collect()
     }
 }
-#[derive(Debug, Clone)]
-struct BasinQueue<'a> {
-    queue: VecDeque<(usize, usize)>,
-    basin: HashSet<(usize, usize)>,
-    grid: &'a Grid,
-}
 
-impl<'a> BasinQueue<'a> {
-    fn new(grid: &'a Grid) -> Self {
-        Self {
-            queue: VecDeque::new(),
-            basin: HashSet::new(),
-            grid
-        }
-    }
-
-    fn checked_push(&mut self, point: (usize, usize)) {
-        if !self.basin.contains(&point) && self.grid.grid[point.1][point.0] < 9 {
-            self.queue.push_back(point);
-            self.basin.insert(point);
-        }
-    }
-
-    fn pop_front(&mut self) -> Option<(usize, usize)> {
-        self.queue.pop_front()
-    }
-}
-
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, PartialEq, Eq)]
 struct Point {
-    height: usize,
+    risk: usize,
     x: usize,
     y: usize,
 }
 
-impl Point {
-    fn risk_level(&self) -> usize {
-        self.height + 1
+impl Ord for Point {
+    fn cmp(&self, other: &Self) -> Ordering {
+        self.risk
+            .cmp(&other.risk)
+            .then_with(|| self.y.cmp(&other.y))
+            .then_with(|| self.x.cmp(&other.x))
+    }
+}
+
+impl PartialOrd for Point {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.cmp(other))
     }
 }
 
@@ -211,16 +228,31 @@ mod tests {
     fn test_part1() {
         let content = read_to_string(PathBuf::from("debug.txt")).unwrap();
         let grid = Grid::read(&content);
-        let low_points = grid.get_low_points();
-        assert_eq!(low_points.len(), 4);
-        assert_eq!(low_points.iter().map(|p| p.risk_level()).sum::<usize>(), 15);
+
+        let parent_cum_risk = grid.get_lowest_risk_paths();
+
+        for row in grid.risk.iter() {
+            for elem in row.iter() {
+                eprint!("{:02} ", elem);
+            }
+            eprintln!();
+        }
+        eprintln!();
+
+        for row in parent_cum_risk.iter() {
+            for elem in row.iter() {
+                if elem.risk < usize::MAX {
+                    eprint!("{:02} ", elem.risk);
+                } else {
+                    eprint!("XX ");
+                }
+            }
+            eprintln!();
+        }
+
+        assert_eq!(parent_cum_risk[grid.size_y - 1][grid.size_x - 1].risk, 40);
     }
 
     #[test]
-    fn test_part2() {
-        let content = read_to_string(PathBuf::from("debug.txt")).unwrap();
-        let grid = Grid::read(&content);
-        println!("{:#?}", grid.get_basin_sizes());
-        assert_eq!(grid.get_largest_basins().iter().product::<usize>(), 1134);
-    }
+    fn test_part2() {}
 }
